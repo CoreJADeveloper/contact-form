cd1eb4982a8278a01f73914220089ca95fac26e3
Tauhidul Alam <like2dev37@gmail.com>
2017-08-19 23:56:41 +0600

    Added plugin file

--- contact-form.php
+++ contact-form.php
@@ -1,7 +1,39 @@
 <?php
+namespace CONTACTFORM;
 /**
- * Created by PhpStorm.
- * User: tauhid
- * Date: 7/13/17
- * Time: 5:00 PM
+ * Plugin Name: WordPress Contact Form
+ * Author: Tauhidul Alam
+ * Description: A contact form :(
  */
+
+
+class ContactForm{
+
+    protected static $_instance = null;
+
+    public static function instance(){
+        if(is_null(self::$_instance)){
+            self::$_instance = new self;
+            self::_initialize_actions();
+        }
+
+        return self::$_instance;
+    }
+
+    private function _initialize_actions(){
+        $this->_enqueue_admin_scripts();
+    }
+
+    private function _enqueue_admin_scripts(){
+
+    }
+
+}
+
+function contact_form(){
+    return ContactForm::instance();
+}
+
+$GLOBALS['contact_form'] = contact_form();
+
+
