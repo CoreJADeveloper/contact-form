9e06852d082c69361e9ffb52e52c17e2d2daaa27
Tauhidul Alam <like2dev37@gmail.com>
2017-08-27 00:57:25 +0600

    Added forms view toggling

--- src/ng-app/forms/form-list/form-list.component.ts
+++ src/ng-app/forms/form-list/form-list.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
     selector: 'form-list',
     templateUrl: './form-list.component.html'
 })
 
-export class FormListViewComponent {
+export class FormListViewComponent implements OnInit {
+    public view_form_list_container: boolean;
 
+    constructor(){
+
+    }
+
+    ngOnInit(): void {
+        this.view_form_list_container = true;
+    }
+
+    toggle_to_add_new(): void {
+        this.view_form_list_container = false;
+    }
+
+    toggle_to_form_list(): void{
+        this.view_form_list_container = true;
+    }
 }
